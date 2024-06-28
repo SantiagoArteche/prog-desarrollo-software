4b51c688b3042f64be431c08327097e5ef54f442
santiago arteche <santiarteche@hotmail.com>
2024-06-28 02:39:41 -0300

    218 fibonacci

---
+++ semana8/Ejercicio218.php
@@ -0,0 +1,53 @@
+<?php
+
+
+
+
+for($i = 0; $i < 20 ;$i++){
+    if($i == 19){
+        $terminos .= fibonacci($i);
+        if(fibonacci($i) % 2 != 0 ){
+            $terminosImpares .= fibonacci($i) ;
+        }
+        
+    }else{
+        $terminos .= fibonacci($i) ." - " ;
+        if(fibonacci($i) % 2 != 0 ){
+            $terminosImpares .= fibonacci($i) . " - " ;
+        }
+    }
+    
+}
+
+for($i = 19; $i >= 0  ; $i--){
+    if($i == 0){
+        $terminosDesc .= fibonacci($i); 
+    }else{
+        $terminosDesc .= fibonacci($i) ." - " ;
+    }
+    
+}
+echo "<h1> Los primeros 20 terminos de la sucesion fibonacci son: $terminos </h1>";
+
+echo "<h1> De los primeros 20 terminos de la sucesion fibonacci los impares son: $terminosImpares </h1>";
+
+echo "<h1> Los primeros 20 terminos de la sucesion fibonacci ordenados de manera descendente son: $terminosDesc </h1>";
+
+function fibonacci($n) {
+    if ($n < 0) {
+        return "Invalid input: Fibonacci is defined for non-negative numbers.";
+    } elseif ($n == 0) {
+        return 0;
+    } elseif ($n == 1) {
+        return 1;
+    } else {
+        return fibonacci($n - 1) + fibonacci($n - 2);
+    }
+}
+
+?>
+
+
+
+
+
--- semana8/README.md
+++ semana8/README.md
@@ -19,3 +19,21 @@ encontrar la distancia recorrida por una aeronave que sale de Londres, Inglaterr
 9:30 p.m. y llega a Bombay, India, a las 11:00 a.m. del siguiente día, suponiendo que
 la nave viaja a 910 km/h y que la diferencia de horarios entre Londres y Bombay es de
 4.5 horas.
+
+# Ejercicio 208.
+
+Realizar un programa HTML en donde se pedirá que ingrese dos palabras. Llamar a
+un programa PHP, que recibirá esas palabras por el método POST. El programa
+deberá llamar a una función que devuelva la cantidad de letras iguales que hay entre
+ambas. Considerar que ninguna de ellas tiene letras repetidas.
+
+# Ejercicio 218.
+
+Realizar un programa PHP que contenga distintas funciones para realizar lo siguiente:
+
+- Deberá generar e informar los primeros 20 términos de la sucesión de
+  Fibonacci (tener en cuenta que los dos primeros términos son iguales a 1 y que
+  los restantes se obtienen como la suma de los dos anteriores).
+- Luego deberá hacer lo mismo, pero ahora no debe imprimir los términos pares.
+- Por último, deberá imprimirla pero al revés, es decir, empezando desde el
+  mayor número, y recorriéndolo en forma descendente.
