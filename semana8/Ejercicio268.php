1f6ab16e1d7a60e897d7321c0f817686208d05a7
santiago arteche <santiarteche@hotmail.com>
2024-07-12 02:57:16 -0300

    E 268

---
+++ semana8/Ejercicio268.php
@@ -0,0 +1,43 @@
+<?php
+
+$cantidadElementos = 50;
+$arrayNumeros = [];
+
+for ($i = 0; count($arrayNumeros) != $cantidadElementos; $i++) {
+    $numerosRandom = rand(1, 1000);
+    if (!in_array($numerosRandom, $arrayNumeros)) {
+        $arrayNumeros[] = $numerosRandom;
+    }
+}
+
+$valores = calculos($arrayNumeros);
+$maxs = $valores[0];
+$mins = $valores[1];
+$promedio = $valores[2];
+
+resultados($maxs, "mayores");
+resultados($mins, "menores");
+echo "<h1>El promedio de todos los numeros del array es de: $promedio</h1>";
+
+
+function calculos($array)
+{
+    sort($array);
+
+    $maxNums = [$array[count($array) - 2], $array[count($array) - 1]];
+
+    $minNums = [$array[0], $array[1]];
+
+    $promedio = array_sum($array) / count($array);
+
+    return [$maxNums, $minNums, $promedio];
+}
+
+function resultados($array, $nombre)
+{
+    echo "<h1>Los dos $nombre números del array son: ";
+    foreach ($array as $valor) {
+        echo "$valor ";
+    }
+    echo "</h1>";
+}
--- semana8/README.md
+++ semana8/README.md
@@ -93,3 +93,11 @@ Ejemplo: para n = 8:
 7  primo
 8  no primo
 Resultando un total de 5 números primos
+
+# Ejercicio 268.
+
+Escribir un programa en PHP que cargue un array de 50 elementos con números al
+azar entre 1 y 1000, y que no se repitan. Luego deberá armar una función que se
+llame Calculos que devolverá los dos mayores números generados, los dos menores
+números generados y el promedio de todos los números del array. Fuera de la función,
+es decir en el script principal de PHP, deberá imprimir esos cinco valores.
